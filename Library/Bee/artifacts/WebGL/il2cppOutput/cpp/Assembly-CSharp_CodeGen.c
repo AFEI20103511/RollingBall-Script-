45e8012c8fa67194e21f0ae4956f1f03dcd14973
John Zhang <ZhangZ1107@gmail.com>
2023-08-31 15:54:50 -0700

    RollingBall

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BallTransform::Start()
+extern void BallTransform_Start_m563D622B59DDCEEA517F3220294401EB82D3A7D1 (void);
+// 0x00000002 System.Void BallTransform::Update()
+extern void BallTransform_Update_m441F920DDE6DAA94714A49576D9661E3C0E9B21F (void);
+// 0x00000003 System.Void BallTransform::.ctor()
+extern void BallTransform__ctor_m482BF52575CBD675EB9D6D37085B645EE0573FE7 (void);
+// 0x00000004 System.Void HelloWorld::Start()
+extern void HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247 (void);
+// 0x00000005 System.Void HelloWorld::Update()
+extern void HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5 (void);
+// 0x00000006 System.Void HelloWorld::.ctor()
+extern void HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35 (void);
+// 0x00000007 System.Void PlaneRotation::Start()
+extern void PlaneRotation_Start_m0DBF6A0CDC6BEAEFE6E4CA54B0D6A1CACF8412F8 (void);
+// 0x00000008 System.Void PlaneRotation::Update()
+extern void PlaneRotation_Update_m77FBD69AD4A341ECD74EE4F53BB395DE06B370C0 (void);
+// 0x00000009 System.Void PlaneRotation::.ctor()
+extern void PlaneRotation__ctor_mB55C180268A1BE9F3DC065A7025DF4AA2479B0B8 (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	BallTransform_Start_m563D622B59DDCEEA517F3220294401EB82D3A7D1,
+	BallTransform_Update_m441F920DDE6DAA94714A49576D9661E3C0E9B21F,
+	BallTransform__ctor_m482BF52575CBD675EB9D6D37085B645EE0573FE7,
+	HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247,
+	HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5,
+	HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35,
+	PlaneRotation_Start_m0DBF6A0CDC6BEAEFE6E4CA54B0D6A1CACF8412F8,
+	PlaneRotation_Update_m77FBD69AD4A341ECD74EE4F53BB395DE06B370C0,
+	PlaneRotation__ctor_mB55C180268A1BE9F3DC065A7025DF4AA2479B0B8,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	967,
+	967,
+	967,
+	967,
+	967,
+	967,
+	967,
+	967,
+	967,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
